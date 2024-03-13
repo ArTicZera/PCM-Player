324d0a76f5aadfefd4dfbc51515c8c410340e11d
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-13 12:46:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -23,35 +23,35 @@ void PlayFrom(DWORD offset)
 
 int main(int argc, char* argv[])
 {
-  FILE* f = fopen("Samples/BadApple.pcm", "rb");
+    FILE* f = fopen("Samples/BadApple.pcm", "rb");
 
-  if (!f)
-  {
-      printf("[-] Couldn't find the file!");
-      return -1;
-  }
+    if (!f)
+    {
+        printf("[-] Couldn't find the file!");
+        return -1;
+    }
 
-  fread(sbuffer, sizeof(BYTE), sizeof(sbuffer), f);
-  fclose(f);
+    fread(sbuffer, sizeof(BYTE), sizeof(sbuffer), f);
+    fclose(f);
 
-  //PlayFrom(1000 * 1000);
+    //PlayFrom(1000 * 1000);
 
-  HWAVEOUT hWaveOut = { 0 };
+    HWAVEOUT hWaveOut = { 0 };
 
-  WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, nSamples, nSamples, 1, 8, 0 };
-  waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, nSamples, nSamples, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
 
-  WAVEHDR header = { (LPSTR)sbuffer, sizeof(sbuffer), 0, 0, 0, 0, 0, 0 };
+    WAVEHDR header = { (LPSTR)sbuffer, sizeof(sbuffer), 0, 0, 0, 0, 0, 0 };
 
-  waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
-  waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
 	
-  waveOutSetVolume(hWaveOut, MAKELONG(VOLUME, VOLUME));
+    waveOutSetVolume(hWaveOut, MAKELONG(VOLUME, VOLUME));
 	
-  waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
-  waveOutClose(hWaveOut);
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
 
-  Sleep(8000 * 1000);
+    Sleep(8000 * 1000);
 
-  return 0;
+    return 0;
 }
